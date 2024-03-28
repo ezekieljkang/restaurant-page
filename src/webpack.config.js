efadbfcb2b395ca6bbff226ebb6254872fc26974
Ezekiel Kang <ezekieljkang@gmail.com>
2024-03-27 18:26:53 -0700

    "added gitignore"
    added gitignore#	new file:   src/webpack.config.js#

---
+++ src/webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = requre('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
